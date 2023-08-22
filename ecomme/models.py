e030fd39734518240f01b6644aeded0a4b06c073
cherubinof <tiossepcherubin@gmail.com>
2023-08-22 23:15:20 +0200

    mise a jour

--- ecomme/models.py
+++ ecomme/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
@@ -58,15 +60,40 @@ class Produit(models.Model):
     nom_produit = models.CharField(max_length=100)
     prix = models.IntegerField()
     rabais = models.IntegerField()
-    info_produit = RichTextField()
+    info_produit = RichTextField(null=True)
     nom_model = models.CharField(max_length=100)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
     tags = models.CharField(max_length=100)
     description = RichTextField()
     section = models.ForeignKey(Section, on_delete=models.DO_NOTHING)
+    slug = models.SlugField(default='', max_length=100, null=True, blank=True)
     
     def __str__(self):
         return self.nom_produit
+    
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse("", kwargs={'slug':self.slug})
+    
+   
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.nom_produit)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Produit.objects.filter(slug=slug).order_by('-id')
+    exists = qs.exists()
+    if exists:
+        new_slug ="%s-%s" % (slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+def pre_save_post_receive(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receive, Produit)
+
 
 class Produit_Image(models.Model):
     produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
